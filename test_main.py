4aba247326c38078c99830fc47f213a736f4414d
baraah-albaalbaki <baraah.albaalbaki@gmail.com>
2021-05-26 17:59:43 +0300

    try failing test

--- test_main.py
+++ test_main.py
@@ -25,7 +25,7 @@ def client():
 def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
-    # assertFalse(response.status_code == 200)
+    assertFalse(response.status_code == 200)
     assert response.json == 'Healthy'
 
 
